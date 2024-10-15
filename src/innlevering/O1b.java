790920c16145b2c8e396981dcb6400c05d148412
ErikBekkemellem <erik.bekkemellem@gmail.com>
2024-10-15 17:46:20 +0200

    fikset O1

---
+++ src/innlevering/O1b.java
@@ -0,0 +1,33 @@
+package innlevering;
+import static javax.swing.JOptionPane.showInputDialog;
+
+public class O1b {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <10; i++) {
+		
+		String poengTxt = showInputDialog("poeng du fikk på prøven: ");
+		
+		int poeng = Integer.parseInt(poengTxt);
+		
+		if(poeng < 0 || poeng > 100) {
+			System.out.println("error");
+		}else if (poeng <=39 && poeng >= 0) {
+			System.out.println("du fikk en karakter F");
+		}else if (poeng <= 49 && poeng >= 40) {
+			System.out.println("du fikk en karakter E");
+		}else if (poeng <= 59 && poeng >= 50) {
+			System.out.println("du fikk en karakter D");
+		}else if (poeng <= 79 && poeng >= 60) {
+			System.out.println("du fikk en karakter C");
+		}else if (poeng <= 89 && poeng >= 80) {
+			System.out.println("du fikk en karakter B");
+		}else if (poeng <= 100 && poeng >= 90) {
+			System.out.println("du fikk en karakter A");
+		}
+
+		}
+	}
+
+}
